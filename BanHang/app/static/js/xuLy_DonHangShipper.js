cb9e24461feaaf2714ab8baa42232ed8745f8df5
Tuan2906 <107672505+Tuan2906@users.noreply.github.com>
2024-02-25 11:41:36 +0700

    Update xuLy_DonHangShipper.js
    Sua Code

--- BanHang/app/static/js/xuLy_DonHangShipper.js
+++ BanHang/app/static/js/xuLy_DonHangShipper.js
@@ -4,10 +4,15 @@ function lay_idDonHang(button)
   layid=row.children[0]
   const ma_vaDon = layid.textContent
  var layma  = ma_vaDon.charAt(4);
-  if (ma_vaDon.charAt(5) >=0)
-  {
-      layma+=ma_vaDon.charAt(5);
-  }
+var j=5;
+ while(j < ma_vaDon.length)
+ {
+       if (ma_vaDon.charAt(j) >=0)
+      {
+          layma+=ma_vaDon.charAt(j);
+          j++;
+      }
+ }
    fetch("/api/xuly_donhang", {
             method: "post",
             body: JSON.stringify({
